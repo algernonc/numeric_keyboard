dce866c3dfce8ba497cba928044162494457c335
algernonc <64118950+algernonc@users.noreply.github.com>
2022-07-26 21:36:51 +0200

    Update numeric_keyboard.dart

--- lib/numeric_keyboard.dart
+++ lib/numeric_keyboard.dart
@@ -115,8 +115,8 @@ class _NumericKeyboardState extends State<NumericKeyboard> {
               image: DecorationImage(
                 image: AssetImage('assets/keyboardkeys/keypadButtonBackground.png'),)),
           alignment: Alignment.center,
-          width: 50,
-          height: 50,
+          // width: 50,
+          // height: 50,
           child: Text(
             value,
             style: TextStyle(
