a3456a9fa7f0245f9baa0b7dea27127c3873f7a1
algernonc <64118950+algernonc@users.noreply.github.com>
2022-07-26 21:47:58 +0200

    Update numeric_keyboard.dart

--- lib/numeric_keyboard.dart
+++ lib/numeric_keyboard.dart
@@ -48,7 +48,7 @@ class _NumericKeyboardState extends State<NumericKeyboard> {
   Widget build(BuildContext context) {
     return Container(
       height: 450,
-      padding: const EdgeInsets.only(top: 35, left: 25, right: 25, bottom: 35),
+      padding: const EdgeInsets.only(bottom: 25, left: 25, right: 25),
       color: Colors.grey.withOpacity(.8),
       alignment: Alignment.center,
       child: Column(
